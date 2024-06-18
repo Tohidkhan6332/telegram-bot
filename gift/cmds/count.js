cd43711e58ed609038039422ecd9052779619dc1
Tohid Khan <tohidkhan9050482152@gmail.com>
2024-06-18 17:38:01 +0530

    Update count.js

--- gift/cmds/count.js
+++ gift/cmds/count.js
@@ -1,6 +1,6 @@
 // ©𝟮𝟬𝟮𝟰
-// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
-// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝐓𝐎𝐇𝐈𝐃 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝐓𝐎𝐇𝐈𝐃𝗠𝗗
 // 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
@@ -19,7 +19,7 @@ if (!gifte.existsSync(messageCountFile)) {
 module.exports = {
     config: {
         name: "count",
-        author: "Gifted Tech",
+        author: "tohid khan",
         description: "Count user messages in the chat",
         category: "ᴜᴛɪʟɪᴛʏ",
         usage: "count [all]",
@@ -75,8 +75,8 @@ module.exports = {
 
 
 // ©𝟮𝟬𝟮𝟰
-// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
-// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝐓𝐎𝐇𝐈𝐃 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝐓𝐎𝐇𝐈𝐃𝗠𝗗
 // 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
