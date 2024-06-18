4d83e46a318cd2cb35b16e9e6ea26de239055507
Tohid Khan <tohidkhan9050482152@gmail.com>
2024-06-18 17:53:09 +0530

    Update gfx1.js

--- gift/cmds/gfx1.js
+++ gift/cmds/gfx1.js
@@ -12,7 +12,7 @@ const axios = require('axios');
 module.exports = {
     config: {
         name: "gfx1",
-        author: "Gifted Tech",
+        author: "Tohid khan",
         description: "Send an image ",
         category: "ʟᴏɢᴏ",
         usage: "<prompt>",
