b51628a961e4efea6e7e7d9111f1661bdf8b1e4f
Tohid Khan <tohidkhan9050482152@gmail.com>
2024-06-18 17:50:49 +0530

    Update gemini.js

--- gift/cmds/gemini.js
+++ gift/cmds/gemini.js
@@ -1,6 +1,6 @@
 // ©𝟮𝟬𝟮𝟰
 // 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
-// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗧.𝗠𝗘/𝐓𝐎𝐇𝐈𝐃𝗠𝗗
 // 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
@@ -11,7 +11,7 @@ const axios = require('axios');
 module.exports = {
     config: {
         name: "gemini",
-        author: "Gifted Tech",
+        author: "Tohid khan",
         description: "Send a response",
         category: "ᴀɪ",
         usage: ".gemini",
@@ -29,7 +29,7 @@ module.exports = {
             const response = await axios.get(apiUrl);
             const giftech = response.data.result;
 
-            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗚𝗘𝗠𝗜𝗡𝗜 𝗔𝗜: \n\n${giftech}`);
+            bot.sendMessage(chatId, `𝐓𝐎𝐇𝐈𝐃-𝗠𝗗 𝗚𝗘𝗠𝗜𝗡𝗜 𝗔𝗜: \n\n${giftech}`);
         } catch (error) {
             console.error('[ERROR]', error);
             bot.sendMessage(chatId, "An error occurred while processing the command.");
